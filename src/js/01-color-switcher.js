dde3d622d44d1b999c46b8e8d251d333d5fd2c37
Dmytro Halytskyi <ds.galytskyi@gmail.com>
2022-11-10 22:30:04 +0200

    add task#1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+refs = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+    body: document.querySelector('body'),
+};
+let intervalID = null;
 
+refs.stopBtn.disabled = true;
+
+refs.startBtn.addEventListener('click', startChangeColor);
+refs.stopBtn.addEventListener('click', stopChangeColor);
+
+function ChangeColor() {
+    refs.body.style.backgroundColor = getRandomHexColor();
+};
+
+function startChangeColor() {
+    intervalID = setInterval(ChangeColor, 1000);
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+};
+
+function stopChangeColor() {
+    clearInterval(intervalID);
+    refs.startBtn.disabled = false;
+    refs.stopBtn.disabled = true;
+};
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
